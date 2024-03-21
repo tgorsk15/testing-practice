e454a6ed31b9882521ccaa257d09ea5ffdb7ada3
tgorsk15 <tgbusiness463@gmail.com>
2024-03-21 08:52:29 -0500

    Pass calculator object tests

--- index.js
+++ index.js
@@ -6,7 +6,27 @@ export function capitalize(string) {
 }
 
 export function reverseString(string) {
-    const splitString = string.split("").reverse()
-    const newString = splitString.join("")
+    const splitString = string.split("").reverse();
+    const newString = splitString.join("");
     return newString
+}
+
+export const calculatorController = function calculator() {
+    function add(a,b) {
+        return a + b
+    };
+
+    function subtract(a, b) {
+        return a - b
+    };
+
+    function multiply(a,b) {
+        return a * b
+    };
+
+    function divide(a,b) {
+        return a / b
+    }
+
+    return {add, subtract, multiply, divide}
 }
--- index.test.js
+++ index.test.js
@@ -1,11 +1,11 @@
-import { capitalize, reverseString } from './index';
+import { capitalize, reverseString, calculatorController } from './index';
 
 console.log(capitalize)
 
 // const capitalize = require('./index.js');
 
 // console.log(capitalize)
-
+const calcRun = calculatorController()
 
 test('capitalize the first letter of a string', () => {
     expect(capitalize('start')).toBe('Start');
@@ -15,3 +15,10 @@ test('reverse the order of a string', () => {
     expect(reverseString('end')).toBe('dne');
     expect(reverseString('did not')).toBe('ton did');
 })
+
+test('perform basic calculator functions', () => {
+    expect(calcRun.add(3, 2)).toBe(5);
+    expect(calcRun.subtract(42, 50)).toBe(-8);
+    expect(calcRun.multiply(3, 4)).toBe(12);
+    expect(calcRun.divide(36, 6)).toBe(6);
+});
